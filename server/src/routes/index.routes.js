c6b9aee4b9043c35ab663d25190aad3f28487d6c
headhtr <christian-gros-pro@outlook.fr>
2023-02-11 18:26:34 +0100

    maj routes

--- server/src/routes/index.routes.js
+++ server/src/routes/index.routes.js
@@ -67,6 +67,38 @@ router.post("/addStory/:id/:utilisateur", async (req, res) => {
 	res.redirect(`http://localhost:5173/joinedPage/${id}/${utilisateur}`);
 });
 
+//a tester
+router.post("/addUser/:id/:utilisateur", async (req, res) => {
+	const { id, utilisateur } = req.params;
+	const { user } = req.body;
+	const parties = await Partie.findById(id);
+
+	let myNewUsers = parties.useres;
+	myNewUsers.push(user);
+
+	const myUpdate = {
+		users: myNewUsers,
+	};
+
+	await Partie.findByIdAndUpdate(id, myUpdate);
+	res.redirect(`http://localhost:5173/joinedPage/${id}/${utilisateur}`);
+});
+
+//a tester
+router.post("/deleteUser/:rang/:id/:utilisateur", async (req, res) => {
+	const { id, rang, utilisateur } = req.params;
+	const parties = await Partie.findById(id);
+	let myNewUser = parties.users;
+	myNewUser.splice(rang, 1);
+
+	const myUpdate = {
+		users: myNewUser,
+	};
+	await Partie.findByIdAndUpdate(id, myUpdate);
+	// res.redirect("http://localhost:5173/");
+	res.redirect(`http://localhost:5173/joinedPage/${id}/${utilisateur}`);
+});
+
 router.get("/parties", async (req, res) => {
 	const parties = await Partie.find();
 	res.json(parties);
